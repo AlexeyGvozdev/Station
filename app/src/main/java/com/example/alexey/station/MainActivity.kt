33148bdf90c03d7f5f8895a02717c35ee5742b8a
Alexey Gvozdev <lesha_chess@mail.ru>
2018-02-10 03:36:29 +0300

    add two fragments, and navigation activity

--- app/src/main/java/com/example/alexey/station/MainActivity.kt
+++ app/src/main/java/com/example/alexey/station/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.alexey.station
 
+//import com.example.alexey.BuildConfig
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val version: String = try {
+            packageManager.getPackageInfo(packageName, 0).versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            "null"
+        }
+
+
+        Toast.makeText(this, BuildConfig.VERSION_NAME, Toast.LENGTH_LONG).show()
+
+        tv_version.text = version
     }
 }
